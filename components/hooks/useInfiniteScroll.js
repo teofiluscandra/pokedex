0e60f095bfc485ad3f9c804f015be7579acef8e1
Teofilus Candra <teofiluschandra@gmail.com>
2022-07-12 16:03:02 +0700

    feat: implement infinite scroll pokedex list homepage

---
+++ components/hooks/useInfiniteScroll.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+
+const useInfiniteScroll = (callback) => {
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    function handleScroll() {
+      if (Math.ceil(window.innerHeight + document.documentElement.scrollTop) !== document.documentElement.offsetHeight || isFetching) return;
+      setIsFetching(true);
+    }
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  useEffect(() => {
+    if (!isFetching) return;
+    callback();
+  }, [isFetching]);
+
+  return [isFetching, setIsFetching];
+};
+
+export default useInfiniteScroll;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
 import Head from 'next/head';
+import { useState } from 'react';
+import useInfiniteScroll from '../components/hooks/useInfiniteScroll';
 import PokemonCard from '../components/PokemonCard';
 
 export default function Home({initialPokemon}) {
+  const [nextPokemonList, setNextPokemonList] = useState(initialPokemon.next);
+  const [pokemon, setPokemon] = useState(initialPokemon.results);
+  const [isFetching, setIsFetching] = useInfiniteScroll(fetchMoreListItems);
+
+  async function fetchMoreListItems() {
+    setIsFetching(true);
+    const response = await fetch(nextPokemonList)
+    const nextPokemon = await response.json()
+    setPokemon([...pokemon, ...nextPokemon.results])
+    setNextPokemonList(nextPokemon.next)
+    setIsFetching(false);
+  }
+
   return (
     <div className="w-full sm:max-w-xl mx-auto space-y-3 bg-white pb-20">
       <Head>
@@ -24,11 +39,12 @@ export default function Home({initialPokemon}) {
             Pokedex ({initialPokemon.count})
           </h1>
           <div className="grid grid-cols-2 gap-10 mt-10">
-            {initialPokemon.results.map((pokemon, index) => (
+            {pokemon.map((pokemon, index) => (
               <PokemonCard key={pokemon.name} pokemon={pokemon} index={index}/>
             ))}
           </div>
         </section>
+        {isFetching && <Loading />}
       </main>
 
       <footer>
@@ -43,6 +59,10 @@ export default function Home({initialPokemon}) {
     </div>
   )
 }
+
+const Loading = () => (
+  <h5 className="font-semibold"><center>Loading ...</center></h5>
+);
 
 export async function getStaticProps() {
   const response = await fetch("https://pokeapi.co/api/v2/pokemon")
